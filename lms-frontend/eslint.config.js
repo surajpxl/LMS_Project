99dbd25f5ff583895a5914082aeb656a7db5bc33
surajpxl <guptasuraj4455@gmail.com>
2025-12-02 03:55:49 +0530

    Started adding basic redux setup with toolkit

--- lms-frontend/eslint.config.js
+++ lms-frontend/eslint.config.js
@@ -1,9 +1,9 @@
-import js from '@eslint/js'
-import { defineConfig, globalIgnores } from 'eslint/config'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import simpleImportSort from 'eslint-plugin-simple-import-sort'
-import globals from 'globals'
+import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -34,4 +34,4 @@ export default defineConfig([
       'semi': ['error', 'always'],
     },
   },
-])
+]);
