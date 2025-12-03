e8cec3495813ae263e432d0b6817f188d6a2776d
surajpxl <guptasuraj4455@gmail.com>
2025-12-04 03:08:24 +0530

    footer component created

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -2,13 +2,16 @@ import './App.css';
 
 import { Routes } from 'react-router-dom';
 
+import Footer from './Components/Footer';
+
 function App() {
 
   return(
     <>
-   <Routes>
+   {/* <Routes>
     
-   </Routes>
+   </Routes> */}
+   <Footer/>
     </>
   );
 }
