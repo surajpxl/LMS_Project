54973c57df236e734618bd7d03e80dd62b88424d
surajpxl <guptasuraj4455@gmail.com>
2025-12-06 23:39:27 +0530

    update

--- lms-frontend/src/Redux/store.js
+++ lms-frontend/src/Redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import authSliceReducer from './Slices/AuthSlice';
+import authSliceReducer from "./Slices/AuthSlice";
 const store = configureStore({
-    reducer: {
-        auth: authSliceReducer
-    },
-    devTools: true
+  reducer: {
+    auth: authSliceReducer,
+  },
+  devTools: true,
 });
 
 export default store;
