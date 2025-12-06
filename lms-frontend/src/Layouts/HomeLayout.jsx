3f2e9a9de5dac91811ae08d6d6993c822a0faf9b
surajpxl <guptasuraj4455@gmail.com>
2025-12-06 23:35:30 +0530

    working on homelayout

--- lms-frontend/src/Layouts/HomeLayout.jsx
+++ lms-frontend/src/Layouts/HomeLayout.jsx
@@ -2,7 +2,13 @@ function HomeLayout(){
 
     return(
         <>
-        
+        <div className="min-h-[90vh]">
+            <div className="drawer absolute left-0 z-50 w-fit">
+<input className="drawer-toggle" id="my-drawer" type="checkbox" />
+<label htmlFor="my-drawer"></label>
+            </div>
+
+        </div>
         </>
     )
 }
