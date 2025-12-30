19bbf6a2b313f2a880a8a901ad273437da1490a0
surajpxl <guptasuraj4455@gmail.com>
2025-12-31 04:09:06 +0530

    some update

--- server/app.js
+++ server/app.js
@@ -29,13 +29,13 @@ app.use('/api/v1/user', userRoutes)
 
 //routes of 3 modules
 
-app.all('/*', (req, res) => {
-    res.status(404).send('OOPS!! 404 page is not found')
-});
-
-// app.use((req, res) => {
-//   res.status(404).send('OOPS!! 404 page is not found');
+// app.all('/*', (req, res) => {
+//     res.status(404).send('OOPS!! 404 page is not found')
 // });
+
+app.use((req, res) => {
+  res.status(404).send('OOPS!! 404 page is not found');
+});
 
 app.use(errorMiddleware)
 
