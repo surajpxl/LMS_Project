d14227a9d2322ff098adb4796dc97fb7ea6095b3
surajpxl <guptasuraj4455@gmail.com>
2026-01-10 04:40:26 +0530

    some work done on payment

--- server/controllers/payment.controller.js
+++ server/controllers/payment.controller.js
@@ -1,18 +1,69 @@
-const getRazorpayApiKey = (req, res, next) => {
+import User from "../models/user.model.js";
+import { razorpay } from "../server.js";
+import AppError from "../utils/error.util.js";
 
-    res.status(200).json({
-        success:true,
-        message:'Razorpay API Key',
-        key: ProcessingInstruction.env.RAZORPAY_KEY_ID
-    })
-}
+const getRazorpayApiKey = async (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    message: "Razorpay API Key",
+    key: process.env.RAZORPAY_KEY_ID,
+  });
+};
 
-const buySubscription = (req, res, next) => {}
+const buySubscription = async (req, res, next) => {
+  const { id } = req.user;
+  const user = await User.findById(id);
+ 
+  if (!user) {
+    return next(new AppError("Unauthorized, please login"));
+  }
 
-const verifySubscription = (req, res, next) => {}
+  if (user.role === "ADMIN") {
+    return next(new AppError("Admin cannot purchase a subscription", 400));
+  }
 
-const cancelSubscription = (req, res, next) => {}
+  const subscription = await razorpay.subscriptions.create({
+    plan_id: process.env.RAZORPAY_PLAN_ID,
+    customer_notify: 1,
+  });
 
-const allPayments = (req, res, next) => {}
+  user.subscription.id = subscription.id;
+  user.subscription.status = subscription.status;
 
-export {getRazorpayApiKey, buySubscription, verifySubscription, cancelSubscription, allPayments}
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Subscribed Successfully",
+    subscription_id: subscription.id,
+  });
+};
+
+
+const verifySubscription =async (req, res, next) => {
+    const {id} = req.user;
+    const {razorpay_payment_id, razorpay_signature, razorpay_subscription_id} = req.body
+
+    const user = await User.findById(id)
+
+    if(!user){
+        return next(
+            new AppError('Unauthorized, please login')
+        )
+    }
+
+    const subscriptionId = user.subscription.id;
+
+};
+
+const cancelSubscription = (req, res, next) => {};
+
+const allPayments = (req, res, next) => {};
+
+export {
+  getRazorpayApiKey,
+  buySubscription,
+  verifySubscription,
+  cancelSubscription,
+  allPayments,
+};
